68d40f127b93d3040ce5f00daa482193dd552d2c
george kavvas <il_gato_nero@hotmail.com>
2022-06-29 10:33:21 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+function loveCalculator(name1, name2) {
+    name1 = prompt("What is your name?");
+    name2 = prompt("What is your crush name?");
+    var loveScore = Math.round(Math.random() * 100);
+    alert("Your Love Score is: " + loveScore + "%");
+}
